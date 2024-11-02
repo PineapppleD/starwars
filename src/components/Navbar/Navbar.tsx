fc1ab04040a0bf82d54b339fc6f6ece7b79984e4
Dias <abdrahmanov.03@inbox.ru>
2024-11-02 21:24:43 +0600

    updated navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logoutAction } from "../../store/authReducer";
-import styles from './style.module.css';
+import styles from "./style.module.css";
 
 const Navbar: React.FC = () => {
   const isAuthenticated = useSelector(
@@ -16,24 +16,24 @@ const Navbar: React.FC = () => {
     navigate("/login");
   };
 
+  const navlinks = ["People", "Planets", "Starships"];
+
   return (
     <nav className={styles.navbar}>
       <ul className={styles.navList}>
-        <li className={styles.navItem}>
-          <NavLink to="/entities/people" className={({ isActive }) => (isActive ? styles.activeLink : styles.navLink)}>
-            People
+        {navlinks.map((link, index) => (
+          <li className={styles.navItem}>
+          <NavLink
+            key={link + index}
+            to={`/entities/${link.toLowerCase()}`}
+            className={({ isActive }) =>
+              isActive ? styles.activeLink : styles.navLink
+            }
+          >
+            {link}
           </NavLink>
-        </li>
-        <li className={styles.navItem}>
-          <NavLink to="/entities/starships" className={({ isActive }) => (isActive ? styles.activeLink : styles.navLink)}>
-            Starships
-          </NavLink>
-        </li>
-        <li className={styles.navItem}>
-          <NavLink to="/entities/planets" className={({ isActive }) => (isActive ? styles.activeLink : styles.navLink)}>
-            Planets
-          </NavLink>
-        </li>
+          </li>
+        ))}
         {isAuthenticated && (
           <li className={styles.navItem}>
             <button className={styles.logoutButton} onClick={handleLogout}>
